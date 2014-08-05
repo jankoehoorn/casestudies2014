8694bf319f1a01ce77a399863bfd37f4b831f657
jankoehoorn <jan.koehoorn@gmail.com>
2014-08-05 20:51:25 +0200

    Changed Title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>Casestudies :: Jan Koehoorn</title>
+		<title>Casestudies :: Jan Koehoorn :: GitHub</title>
 
 		<?php
 			require_once 'elements/header-required.php';
